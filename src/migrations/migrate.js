12dc4e3fd6f5b89ccfa15f63dd94c907a1a925aa
Rahul Sharma <rahul.fabled@gmail.com>
2020-08-16 21:34:17 +0545

    update migration file to push to heroku

--- src/migrations/migrate.js
+++ src/migrations/migrate.js
@@ -3,7 +3,7 @@ import * as users from './create_users_table';
 import * as todos from './create_todos_table';
 
 // users.down(client);
-users.up(client);
+// users.up(client);
 
 // todos.down(client);
-// todos.up(client);
+todos.up(client);
